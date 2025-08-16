0a2abcb94d39a9416df28a6d13d6bbb728991ae4
kshiteej dubey <kddubey8770@gmail.com>
2025-08-16 17:56:28 +0530

    Fix backend dependencies for Render

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI || "mongodb://localhost:27017/project_tool";
+  const uri = process.env.MONGODB_URI) || "mongodb://localhost:27017/project_tool";
   try {
     await mongoose.connect(uri);
     console.log("MongoDB connected");
